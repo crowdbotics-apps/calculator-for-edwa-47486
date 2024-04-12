7feabf7893fb61cad0b21c67853f4b06bf4e4567
Crowdbotics <team@crowdbotics.com>
2024-04-12 11:33:02 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "calculator-for-edwa-47486",
+  url: "https://calculator-for-edwa-47486.botics.co",
+  api: "https://calculator-for-edwa-47486.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
